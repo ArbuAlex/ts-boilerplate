b156ab33d60c42558c19d5780cbf2333e127f9ba
Claire Blasphemous <cblasphe@il-f5.msk.21-school.ru>
2020-09-14 22:34:37 +0300

    test echo bot

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-'use strict'
-console.log('Helllo world');
+import { Telegraf } from 'telegraf'
+import { exit } from 'process'
+
+const botToken = process.env.BOT_TOKEN
+if (!botToken) exit()
+const bot = new Telegraf(botToken)
+
+bot.hears(/.*/, (ctx) => {
+    const text = ctx.message?.text ||"NO text argument" 
+    ctx.reply(text);
+})
+
+bot.launch()
